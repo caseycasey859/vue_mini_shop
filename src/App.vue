2754adbde85fdd4f5fc34914f7490a579cbee29f
casey casey <caseycasey859@gmail.com>
2024-08-06 10:27:21 +0900

    라우터_레이아웃설정2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <HeaderArea/>
-  
     <router-view/>
     <FooterArea/>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    여기는 홈뷰입니다.
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
